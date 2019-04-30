ed27e1329b3ffa3e682552c6358a5d5c0b152cd7
henriquemats <henrique.projects28@gmail.com>
2019-04-30 13:43:29 -0300

    feat: start text robot

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const readline = require("readline-sync");
 
-function start() {
+const robots = {
+  text: require("./robots/text")
+};
+
+async function start() {
   const content = {};
 
   content.searchTerm = askAndReturnSearchTerm();
   content.prefix = askAndReturnPrefix();
+
+  await robots.text(content);
 
   function askAndReturnSearchTerm() {
     return readline.question("Type a Wikipedia search term: ");
